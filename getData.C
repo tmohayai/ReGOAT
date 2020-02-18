89c5d64b76a9f345fc57e9c4c3ec6aeb43a513c8
Tanaz Mohayai <tmohayai@users.noreply.github.com>
2020-02-18 08:10:41 -0600

    fixed a memory leak

--- getData.C
+++ getData.C
@@ -15,7 +15,7 @@
 #include <string>
 #include <vector>
 
-//some ROOT includes
+/*//some ROOT includes
 #include "TInterpreter.h"
 #include "TROOT.h"
 #include "TH1F.h"
@@ -26,7 +26,8 @@
 #include "TLegend.h"
 #include "TStopwatch.h"
 #include "TString.h"
-
+*/
+//"art" includes (canvas, and gallery)
 #include "canvas/Utilities/InputTag.h"
 #include "gallery/Event.h"
 #include "gallery/ValidHandle.h"
@@ -34,13 +35,8 @@
 #include "canvas/Persistency/Common/FindOne.h"
 #include "canvas/Persistency/Common/FindManyP.h"
 #include "canvas/Persistency/Common/FindOneP.h"
-
 #include "lardataobj/RawData/RawDigit.h"
 #include "lardataobj/RawData/raw.h"
-
-
-//print events?
-bool printEvent = false;
 
 //global struct for hits
 struct hit_t {
@@ -109,6 +105,7 @@ void createBranches(TTree* hitSumm, hit_t &hit, const std::vector <Int_t> &chIDs
         branchName <<"signal_" << chIDs[i];
         branchSpec << branchName.str().c_str() << "[" <<recordLength << "]/S";
         hitSumm->Branch(branchName.str().c_str(), &(hit.signal), branchSpec.str().c_str());
+        //   hitSumm->Branch("signal", &(hit.signal), "signal[3,3072]/I");
     }
     
 }
@@ -159,122 +156,89 @@ void refreshTreeAddresses(TTree *hitSumm, hit_t &hit, const std::vector <Int_t> 
         hitSumm->SetBranchAddress(branchName.str().c_str(),&(hit.signal[i][0]));
     }
 }
-// ---------------------- This function does most of the work! ------------------------ //
-// ------------------- It processes events for each daq trigger  -------------------- //
+
+
 
 void ProcessEvent(gallery::Event *ev, TFile *outfile, std::string filenamebase, const std::vector <Int_t> &chIDs,TTree *hitSumm, TTree *config, hit_t &hit)
 {
-    std::cout << "Processing "
-    << "Run " << ev->eventAuxiliary().run() << ", "
-    << "Event " << ev->eventAuxiliary().event() << std::endl;
-    
-    // The Tags struct contains the InputTags that identify the products
-    // we'll read from the gallery::Event. They are the same InputTags
-    // as are used in art modules. The string specified is the label of
-    // the module that produced the product in question
-    // This is a parameter from the fhicl file in Calibration/CalWireROIT1034_module.cc
-    // The default value is "daq"
-    art::InputTag const fDigitModuleLabel("daq");
-    
-    // Read in the digit List object(s)
-    auto const& digitVecHandle = ev->getValidHandle<std::vector<raw::RawDigit>>(fDigitModuleLabel);
-    
-    // This is the number of channels we have read out
-    const int nchannels = (int)digitVecHandle->size();
-    //std::cout << "Size of digitVecHandle is " << nchannels << std::endl;
-    
-    // Now set up some variables we'll need later and set them to dummy values
-    unsigned int dataSize = 0; // size of raw data vectors
-    raw::ChannelID_t channel = raw::InvalidChannelID; // channel number
-    unsigned int bin(0); // time bin loop variable
-    
-    // recordLength is the recordLength set in the config file.
-    // Set to a 3072 value for now; get it from digitVec.Samples for each event
-    const int recordLength = 3072; // change this (and its name!)
-    std::vector<short> rawadc(recordLength); // vector holding uncompressed adc values
-    outfile->cd();
-    TH1F *waveforms[nchannels];
-    TString histname;
-    TString histtitle;
-    // Loop over all channels
-    for (int i_ch = 0; i_ch < nchannels; i_ch++) {
+	std::cout << "Processing "
+	<< "Run " << ev->eventAuxiliary().run() << ", "
+	<< "Event " << ev->eventAuxiliary().event() << std::endl;
+	art::InputTag const fDigitModuleLabel("daq");
+
+	auto const& digitVecHandle = ev->getValidHandle<std::vector<raw::RawDigit>>(fDigitModuleLabel);
+
+ 	const int nchannels = (int)digitVecHandle->size();
+    	std::cout << "Size of digitVecHandle is " << nchannels << std::endl;
+	unsigned int dataSize = 0; // size of raw data vectors
+    	raw::ChannelID_t channel = raw::InvalidChannelID; // channel number
+    	unsigned int bin(0); // time bin loop variable
+	const int recordLength = 3072;
+    	std::vector<short> rawadc(recordLength);
+ 	TH1F *waveforms[nchannels];
+
+	for (int i_ch = 0; i_ch < nchannels; i_ch++) {
         
-        // Get the reference to the current raw::RawDigit
         auto const& digitVec = (*digitVecHandle)[i_ch];
         
-        // What channel are we looking at?
         channel = digitVec.Channel();
-        //std::cout << "i_ch is " << i_ch << ", and channel is " << channel << std::endl;
-        
-        // How many time ticks do we have (recordLength)?
         dataSize = digitVec.Samples();
-        // Resize rawadc to this size
         rawadc.resize(dataSize);
-        
-        // Instantiate histogram
+        TString histname;
+    	TString histtitle;
         waveforms[i_ch] = new TH1F(histname.Data(),histtitle.Data(),dataSize+1,-0.5,dataSize+0.5);
-     	histname = Form("h_%d",i_ch);
-	histtitle = Form("Channel %d;Time tick;ADC",i_ch);   
-        // Uncompress data
+        
         raw::Uncompress(digitVec.ADCs(), rawadc, digitVec.Compression());
         
-        // Loop over all adc values and put them in the histogram
         for (bin = 0; bin < dataSize; bin++){
             waveforms[i_ch]->SetBinContent(bin+1,rawadc[bin]);
         }
         
-    } // loop over all channels (i_ch)
-    
-    
-    
-    // First, cd to output file and make a TDirectory for this event
-    int evtnum = ev->eventAuxiliary().event();
-    TString dirname = Form("evt_%d/",evtnum);
-    
-    
-    
-    //other vars
-    Double_t evEnergy = -1;
-    Double_t noiseStd = -1;
-    Double_t sigOffset = -1;
-    
-    int bin_ch;
-    double canvmax = 1.3;
-    double canvmin = -1.3;
-    
-    //resize hit variable and update tree branches. Load single variables
-    hit.reset();
-    hit.resize(chIDs.size(),dataSize);
-    hit.eID = evtnum;
-    hit.nCh = chIDs.size();
-    hit.nSamp = dataSize;
-    for (UShort_t it = 0; it<dataSize; it++) {
-        hit.t[it] = it;
-    }
-    
-    // Draw histograms in groups of 16 to make colours not insane
+	}
+
+	int evtnum = ev->eventAuxiliary().event();
+    	TString dirname = Form("evt_%d/",evtnum);
+	
+	Double_t evEnergy = -1;
+ 	Double_t noiseStd = -1;
+    	Double_t sigOffset = -1;
+
+    	int bin_ch;
+    	double canvmax = 1.3;
+    	double canvmin = -1.3;
+
+	hit.reset();
+    	hit.resize(chIDs.size(),dataSize);
+    	hit.eID = evtnum;
+    	hit.nCh = chIDs.size();
+    	hit.nSamp = dataSize;
+    	//for (UShort_t it = 0; it<dataSize; it++) {
+        //	hit.t[it] = it;
+	//}
+
+
     for (size_t i_ch = 0; i_ch<chIDs.size();i_ch++) {
-        
-        
+
+
         hit.chID[i_ch] = chIDs[i_ch];
-        
-        // Calculate nosie, baseline and event energy
-        //use first 20% of the sample to calculate noise statistics
-        Int_t nBins = waveforms[chIDs[i_ch]]->GetNbinsX();
+        canvmax = 2;
+        canvmin = -2;
+
+ 	Int_t nBins = waveforms[chIDs[i_ch]]->GetNbinsX();
         Int_t finBinNoise = nBins*2/10;
         Int_t initBinNoise = 1;
         sigOffset = waveforms[chIDs[i_ch]]->Integral(initBinNoise,finBinNoise)/(finBinNoise - initBinNoise + 1);
         evEnergy = fabs(waveforms[chIDs[i_ch]]->Integral() - nBins*sigOffset);
-        hit.E[i_ch] = evEnergy;;
+           hit.E[i_ch] = evEnergy;;
         hit.offset[i_ch] = sigOffset;
-        for (Int_t i = initBinNoise; i < finBinNoise + 1; ++i) {
+
+	for (Int_t i = initBinNoise; i < finBinNoise + 1; ++i) {
             noiseStd += pow(waveforms[chIDs[i_ch]]->GetBinContent(i) - sigOffset,2);
         }
-        noiseStd = sqrt(noiseStd/double(finBinNoise-initBinNoise+1));
+	noiseStd = sqrt(noiseStd/double(finBinNoise-initBinNoise+1));
         hit.noise[i_ch] = noiseStd;
-        
-        //get position and value of maximum and minimum of channels
-        Int_t maxPos = waveforms[chIDs[i_ch]]->GetMaximumBin();
+
+	Int_t maxPos = waveforms[chIDs[i_ch]]->GetMaximumBin();
         Double_t maxValue = waveforms[chIDs[i_ch]]->GetBinContent(maxPos);
         hit.maxPos[i_ch] = maxPos;
         hit.maxValue[i_ch] = maxValue;
@@ -282,13 +246,10 @@ void ProcessEvent(gallery::Event *ev, TFile *outfile, std::string filenamebase, 
         Double_t minValue = waveforms[chIDs[i_ch]]->GetBinContent(minPos);
         hit.minPos[i_ch] = minPos;
         hit.minValue[i_ch] = minValue;
-        
-        //get tmin tmax and Energy between points
-        Int_t tInit = waveforms[chIDs[i_ch]]->FindFirstBinAbove(hit.offset[i_ch]-3*hit.noise[i_ch]);
+	Int_t tInit = waveforms[chIDs[i_ch]]->FindFirstBinAbove(hit.offset[i_ch]-3*hit.noise[i_ch]);
         Int_t tFin = waveforms[chIDs[i_ch]]->FindLastBinAbove(hit.offset[i_ch]-3*hit.noise[i_ch]);
-        //Int_t tInit = -1;
-        //Int_t tFin = -1;
-        Bool_t flagAboveThres = false;
+
+	Bool_t flagAboveThres = false;
         for (Int_t i = 1; i < nBins+1; ++i) {
             if (waveforms[chIDs[i_ch]]->GetBinContent(i)<hit.offset[i_ch]-5*hit.noise[i_ch]){
                 if (!flagAboveThres){
@@ -298,14 +259,11 @@ void ProcessEvent(gallery::Event *ev, TFile *outfile, std::string filenamebase, 
                 tFin = i;
             }
         }
-        Double_t E0 = fabs(waveforms[chIDs[i_ch]]->Integral(tInit,tFin,"width") - (tFin-tInit+1)*sigOffset);
+	Double_t E0 = fabs(waveforms[chIDs[i_ch]]->Integral(tInit,tFin,"width") - (tFin-tInit+1)*sigOffset);
         hit.tInit[i_ch] = tInit;
         hit.tFin[i_ch] = tFin;
         hit.E0[i_ch] = E0;
-        
-        
-        //maximum derivative
-        Double_t slope = 0;
+	Double_t slope = 0;
         Double_t minSlope = 0;
         Int_t minSlopePos = 0;
         for (Int_t i = 1; i < hit.nSamp-1; ++i) {
@@ -315,34 +273,25 @@ void ProcessEvent(gallery::Event *ev, TFile *outfile, std::string filenamebase, 
                 minSlopePos = i;
             }
         }
-        hit.minSlope[i_ch] = minSlope;
+	hit.minSlope[i_ch] = minSlope;
         hit.tMinSlope[i_ch] = minSlopePos;
-        
-        
-        //save event signal
-        for (int bin = 0; bin < hit.nSamp; bin++){
+	for (int bin = 0; bin < hit.nSamp; bin++){
             hit.signal[i_ch][bin] = (Short_t) waveforms[chIDs[i_ch]]->GetBinContent(bin+1);
         }
-        
 
-        
-        
     }
-    
-    //refresh branches addresses and fill
+
     refreshTreeAddresses(hitSumm,hit, chIDs);
     hitSumm->Fill();
-    
-    
+
     for (int i_ch = 0; i_ch < nchannels; i_ch++) {
         delete waveforms[i_ch];
     }
+
+
+
 }
-
-
-// ---------------------- This is the main function ------------------------ //
-
-void getData(std::string input_files, std::string outfilename="data.root")
+void test(std::string input_files, std::string outfilename="data.root")
 {
     gStyle->SetOptStat(0);
     
@@ -355,7 +304,8 @@ void getData(std::string input_files, std::string outfilename="data.root")
     
     //Channel of interest
     std::vector <Int_t> chIDs;
-    chIDs.push_back(32);
+    
+    chIDs.push_back(33);
     
     //create event struct and tree branches
     hit_t hit;
@@ -367,12 +317,6 @@ void getData(std::string input_files, std::string outfilename="data.root")
     // Format our files list
     std::vector<std::string> filenames = GetFileList(input_files);
     
-    // The gallery::Event object acts as a cursor into the stream of
-    // events. A newly-constructed gallery::Event is at the start of
-    // its stream. Use gallery::Event::atEnd() to check if you've
-    // reached the end of the stream. Use gallery::Event::next() to go
-    // to the next event.
-    //
     // Make a gallery::Event
     gallery::Event ev(filenames);
     
@@ -383,15 +327,12 @@ void getData(std::string input_files, std::string outfilename="data.root")
         ev.next();
     }
     
-    
-    //save treees and output file
     outfile->cd();
     hitSumm->SetBranchStatus("*",1);
     hitSumm->Write("",TObject::kOverwrite);
     outfile->Close();
     
     //delete outfile;
+    outfile->Delete();
     cout << "DONE!" << endl;
-    usleep(1000000);
-    cout << "there may be a segfault after this but the processing is usually un-affected by this" << endl << endl << endl;
 }
