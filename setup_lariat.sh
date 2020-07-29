78365fb8ce4a52371c3ac33631f870458b195cf8
Tanaz Mohayai <tmohayai@users.noreply.github.com>
2020-07-29 14:40:59 -0500

    updated the setup script

--- setup_lariat.sh
+++ setup_lariat.sh
@@ -2,15 +2,12 @@
 # note: with a recent upgrade to dunegpvms, only sl7 would work for setting up LArIATSoft. 
 # Run this set up script after each login once a LArIATSoft development area is set up
 
-cd to_your_lariat_directory/
 source /grid/fermiapp/larsoft/products/setup
 export PRODUCTS=/grid/fermiapp/products/lariat:${PRODUCTS}:/grid/fermiapp/products/common/db
 setup git
 setup gitflow
 setup mrb
 setup ninja v1_6_0b
-
 source localProducts_lariatsoft_develop_e14_prof/setup
 mrbsetenv
 setup lariatsoft v06_62_00 -q e14:prof
-setup gallery v1_05_03 -q e14:nu:prof
